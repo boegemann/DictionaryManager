307d0dce9a3c8957e7aac594a3d344f914dfa277
ibogemann <ingo@whiteclarkegroup.com>
2017-07-07 10:31:51 +0100

    Logon the second

--- src/index.js
+++ src/index.js
@@ -4,38 +4,41 @@ import {createStore, applyMiddleware} from 'redux'
 import reducer from './reducers'
 import fetch from 'isomorphic-fetch'; // so I can use fetch()
 import Root from './components/Root'
-import jwtDecode from  'jwt-decode' ;
 import thunkMiddleware from 'redux-thunk'
 let createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore)
 
 
 //const url='https://dictionayryservices.herokuapp.com/APP';
-const url = 'http://localhost:3001/APP';
+const url = 'http://localhost:3001';
+const appService = "/APP";
 
-fetch(url)
-  .then(function (response) {
-    if (response.status >= 400) {
-      throw new Error("Bad response from server");
-    }
-    return response.json();
+
+// we start off by seeing whether we have a JWT token stored
+let access_token = localStorage.getItem("access_token");
+
+// Now call the server to get the inital application state
+fetch(url + appService, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    access_token: access_token
   })
-  .then(function (initialState) {
-    let username = localStorage.getItem("user_name");
-    let access_token = localStorage.getItem("access_token");
-    if (access_token) {
-      let jwt = jwtDecode(access_token);
-      console.log(jwt);
-      if (jwt.exp * 1000 > (new Date().getTime())) {
-        initialState.auth.isAuthenticated = true;
-      }
-
-    }
-    document.title = initialState.app.title;
-    let store = createStoreWithMiddleware(reducer, initialState);
-    render(
-      <Root store={store}/>,
-      document.getElementById('root')
-    );
-  });
+}).then(function (response) {
+  console.log(response)
+  if (response.status >= 400) {
+    throw new Error("Bad response from server");
+  }
+  return response.json();
+}).then(function (initialState) {
+  console.log(initialState)
+  document.title = initialState.app.title;
+  let store = createStoreWithMiddleware(reducer, initialState);
+  render(
+    <Root store={store}/>,
+    document.getElementById('root')
+  );
+});
 
 
