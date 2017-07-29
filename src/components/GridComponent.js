d14f194217f810905ab7c74de3f59703bbb83f99
ibogemann <ingo@whiteclarkegroup.com>
2017-07-29 22:52:20 +0100

    Changed to Dev Extreme Grid

--- src/components/GridComponent.js
+++ src/components/GridComponent.js
@@ -1,18 +1,56 @@
+// import React from 'react';
+// import {Grid} from 'react-redux-grid';
+//
+//
+// class GridComponent extends React.Component {
+//
+//   render() {
+//     let {data, columndef, events, unitKey} = this.props
+//     return <Grid
+//       data={data}
+//       stateKey={unitKey}
+//       columns={columndef}
+//       events={events}
+//       plugins={{}}/>
+//   };
+// }
+//
+// export default GridComponent;
 import React from 'react';
-import {Grid} from 'react-redux-grid';
 
+import {
+  SortingState,
+  LocalSorting,
+} from '@devexpress/dx-react-grid';
+
+import {
+  Grid, TableView, TableHeaderRow
+} from '@devexpress/dx-react-grid-material-ui';
 
 class GridComponent extends React.Component {
 
   render() {
     let {data, columndef, events, unitKey} = this.props
-    return <Grid
-      data={data}
-      stateKey={unitKey}
-      columns={columndef}
-      events={events}
-      plugins={{}}/>
-  };
-}
+    console.log(columndef)
 
+    let coldef = columndef.map((cd) => {
+      return {
+        name: Array.isArray(cd.dataIndex)?cd.dataIndex.join("."):cd.dataIndex,
+        title: cd.name
+      }
+    });
+
+    console.log(coldef)
+
+    return <Grid
+      rows={data}
+      columns={coldef}>
+      <SortingState />
+      <LocalSorting />
+      <TableView />
+      <TableHeaderRow  allowSorting />
+    </Grid>
+  };
+
+}
 export default GridComponent;
