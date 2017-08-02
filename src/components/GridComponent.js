e5534c81364ca0f28a5c02386d902de3f9d0cfdc
ibogemann <ingo@whiteclarkegroup.com>
2017-08-02 09:42:31 +0100

    Delete now working as well

--- src/components/GridComponent.js
+++ src/components/GridComponent.js
@@ -11,59 +11,70 @@ import DeleteIcon from 'material-ui-icons/Delete';
 class GridComponent extends React.Component {
 
 
-  render() {
-    let {gridDefinition, data, columndef, events, unitKey, selection, edit} = this.props;
-    const pageSize = 5;
+    render() {
+        let {gridDefinition, data, columndef, events, unitKey, selection, edit, add, remove} = this.props;
+        const pageSize = 5;
 
-    const fEdit = ()=>{edit(selection,gridDefinition.edit)}
+        const fEdit = () => {
+            edit(selection, gridDefinition.edit)
+        };
+        const fAdd = () => {
+            add(gridDefinition.add)
+        };
+        const fRemove = () => {
+            remove(selection, gridDefinition.remove)
+        };
 
-    return <div>
+        return <div>
 
-      <Toolbar>
-        <div>
-          <Typography type="title">{gridDefinition.title}</Typography>
+            <Toolbar>
+                <div>
+                    <Typography type="title">{gridDefinition.title}</Typography>
+                </div>
+                <div className="grid_spacer"/>
+                <div className="grid_actions">
+
+                    {gridDefinition.add != null && (
+                        <IconButton aria-label="Add Item" onTouchTap={fAdd}>
+                            <AddIcon/>
+                        </IconButton>)
+                    }
+                    {gridDefinition.edit != null && selection != null && (
+                        <IconButton aria-label="Edit Item" onTouchTap={fEdit}>
+                            <EditIcon/>
+                        </IconButton>)
+                    }
+                    {gridDefinition.remove != null && selection != null && (
+                        <IconButton aria-label="Delete Item" onTouchTap={fRemove}>
+                            <DeleteIcon/>
+                        </IconButton>)
+                    }
+                </div>
+            </Toolbar>
+
+
+            <Grid
+                data={data}
+                stateKey={unitKey}
+                columns={columndef}
+                events={events}
+                pageSize={pageSize}
+                height={false}
+                plugins={{
+                    PAGER: {
+                        enabled: true,
+                        pagingType: 'local',
+                        toolbarRenderer: (pageIndex, pageSize, total, currentRecords, recordType) => {
+                            return `${pageIndex * pageSize} through ${pageIndex * pageSize + currentRecords} of ${total} ${recordType} Displayed`;
+                        },
+                        pagerComponent: false
+                    }
+                }}/>
         </div>
-        <div className="grid_spacer"/>
-        <div className="grid_actions">
 
-          <IconButton aria-label="Filter list">
-            <AddIcon />
-          </IconButton>
-          {gridDefinition.delete!=null && selection!=null && (
-            <IconButton aria-label="Filter list">
-              <DeleteIcon />
-            </IconButton>)
-          }
-          {gridDefinition.edit!=null  && selection!=null && (
-            <IconButton aria-label="Filter list" onTouchTap={fEdit}>
-              <EditIcon />
-            </IconButton>)
-          }
-        </div>
-      </Toolbar>
-
-
-      <Grid
-        data={data}
-        stateKey={unitKey}
-        columns={columndef}
-        events={events}
-        pageSize={pageSize}
-        height={false}
-        plugins={{
-          PAGER: {
-            enabled: true,
-            pagingType: 'local',
-            toolbarRenderer: (pageIndex, pageSize, total, currentRecords, recordType) => {
-              return `${pageIndex * pageSize} through ${pageIndex * pageSize + currentRecords} of ${total} ${recordType} Displayed`;
-            },
-            pagerComponent: false
-          }
-        }}/>
-    </div>
-
-  };
+    };
 }
+
 export default GridComponent;
 
 
--- src/containers/GridContainer.js
+++ src/containers/GridContainer.js
@@ -4,87 +4,99 @@ import {callService} from '../actions/screen'
 import {rowSelected} from '../actions/grid'
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    edit: (selection, editDef) => {
-      let service = editDef.service;
-      delete editDef.service;
-      let params = {rowData: selection, eventInfo: editDef};
-      dispatch(callService(service, params));
-    },
-    events: {
-      HANDLE_CELL_CLICK: () => {
-      },
-      HANDLE_CELL_DOUBLE_CLICK: () => {
-      },
-      HANDLE_BEFORE_ROW_CLICK: () => {
-      },
-      HANDLE_ROW_CLICK: () => {
-      },
-      HANDLE_ROW_DOUBLE_CLICK: ({row}, event) => {
-        if (ownProps.hasOwnProperty("events") && ownProps.events.hasOwnProperty("row:doubleClick")) {
-          let service = ownProps.events["row:doubleClick"].service;
-          let params = {rowData: {...row}, eventInfo: {...ownProps.events["row:doubleClick"]}};
-          delete params.eventInfo.service;
-          dispatch(callService(service, params));
-        }
-      },
-      HANDLE_BEFORE_SELECTION: () => {
-      },
-      HANDLE_AFTER_SELECTION: ({selected, data}) => {
-        dispatch(rowSelected(ownProps.unitKey, data, selected));
+    return {
+        edit: (selection, editDef) => {
+            let service = editDef.service;
+            delete editDef.service;
+            let params = {rowData: selection, eventInfo: editDef};
+            dispatch(callService(service, params));
+        },
+        add: (addDef) => {
+            let service = addDef.service;
+            delete addDef.service;
+            let params = {eventInfo: addDef};
+            dispatch(callService(service, params));
+        },
+        remove: (selection, removeDef) => {
+            let service = removeDef.service;
+            delete removeDef.service;
+            let params = {rowData: selection, eventInfo: removeDef};
+            dispatch(callService(service, params));
+        },
+        events: {
+            HANDLE_CELL_CLICK: () => {
+            },
+            HANDLE_CELL_DOUBLE_CLICK: () => {
+            },
+            HANDLE_BEFORE_ROW_CLICK: () => {
+            },
+            HANDLE_ROW_CLICK: () => {
+            },
+            HANDLE_ROW_DOUBLE_CLICK: ({row}, event) => {
+                if (ownProps.hasOwnProperty("events") && ownProps.events.hasOwnProperty("row:doubleClick")) {
+                    let service = ownProps.events["row:doubleClick"].service;
+                    let params = {rowData: {...row}, eventInfo: {...ownProps.events["row:doubleClick"]}};
+                    delete params.eventInfo.service;
+                    dispatch(callService(service, params));
+                }
+            },
+            HANDLE_BEFORE_SELECTION: () => {
+            },
+            HANDLE_AFTER_SELECTION: ({selected, data}) => {
+                dispatch(rowSelected(ownProps.unitKey, data, selected));
 
-      },
-      HANDLE_BEFORE_INLINE_EDITOR_SAVE: () => {
-      },
-      HANDLE_AFTER_INLINE_EDITOR_SAVE: () => {
-      },
-      HANDLE_BEFORE_BULKACTION_SHOW: () => {
-      },
-      HANDLE_AFTER_BULKACTION_SHOW: () => {
-      },
-      HANDLE_BEFORE_SORT: () => {
-      },
-      HANLE_BEFORE_EDIT: () => {
-      },
-      HANDLE_AFTER_SELECT_ALL: () => {
-      },
-      HANDLE_AFTER_DESELECT_ALL: () => {
-      },
-      HANDLE_AFTER_ROW_DROP: () => {
-      },
-      HANDLE_BEFORE_TREE_CHILD_CREATE: () => {
-      },
-      HANDLE_EDITOR_FOCUS: () => {
-      },
-      HANDLE_EDITOR_BLUR: () => {
-      }
+            },
+            HANDLE_BEFORE_INLINE_EDITOR_SAVE: () => {
+            },
+            HANDLE_AFTER_INLINE_EDITOR_SAVE: () => {
+            },
+            HANDLE_BEFORE_BULKACTION_SHOW: () => {
+            },
+            HANDLE_AFTER_BULKACTION_SHOW: () => {
+            },
+            HANDLE_BEFORE_SORT: () => {
+            },
+            HANLE_BEFORE_EDIT: () => {
+            },
+            HANDLE_AFTER_SELECT_ALL: () => {
+            },
+            HANDLE_AFTER_DESELECT_ALL: () => {
+            },
+            HANDLE_AFTER_ROW_DROP: () => {
+            },
+            HANDLE_BEFORE_TREE_CHILD_CREATE: () => {
+            },
+            HANDLE_EDITOR_FOCUS: () => {
+            },
+            HANDLE_EDITOR_BLUR: () => {
+            }
+        }
     }
-  }
 };
 
 const mapStateToProps = (state, ownProps) => {
-  let selection = state.selection;
-  let unitKey = ownProps.unitKey;
-  let selectedIds = (selection != null && selection.get(unitKey) != null && selection.get(unitKey).get('indexes') != null)
-    ? selection.get(unitKey).get('indexes')
-    : [];
+    let selection = state.selection;
+    let unitKey = ownProps.unitKey;
+    let selectedIds = (selection != null && selection.get(unitKey) != null && selection.get(unitKey).get('indexes') != null)
+        ? selection.get(unitKey).get('indexes')
+        : [];
 
-  // Bit of a duplication but selected ids can't give us row data and grid selection event doesn't clear data when we leave
-  let selectionProp = selectedIds.length > 0 && state.gridSelection !== null ? state.gridSelection[unitKey] : null;
-  console.log(selection)
+    // Bit of a duplication but selected ids can't give us row data and grid selection event doesn't clear data when we leave
+    let selectionProp = selectedIds.length > 0 && state.gridSelection !== null ? state.gridSelection[unitKey] : null;
+    console.log(selection)
 
-  return {
-    gridDefinition: state.screen[ownProps.unitIndex].grid,
-    unitKey: ownProps.unitKey,
-    data: ownProps.colDefAndData.data,
-    columndef: ownProps.colDefAndData.columndef,
-    selection: selectionProp
-  };
+    return {
+        gridDefinition: state.screen[ownProps.unitIndex].grid,
+        unitKey: ownProps.unitKey,
+        data: ownProps.colDefAndData.data,
+        columndef: ownProps.colDefAndData.columndef,
+        selection: selectionProp
+    };
 };
 
 const GridContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(GridComponent);
 
 export default GridContainer;
