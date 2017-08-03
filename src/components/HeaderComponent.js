78d53fdc566a7d9b8cff987b47ba4fb76187c4dc
ibogemann <ingo@whiteclarkegroup.com>
2017-08-03 08:52:29 +0100

    Logout

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import ExitToAppIcon from 'material-ui-icons/ExitToApp';
 import Typography from 'material-ui/Typography';
 
-const HeaderComponent = ({title}) => (
-<AppBar position="fixed" color="default">
-  <Toolbar>
-    <Typography type="title" color="inherit">
-      {title != null ? title : ""}
-    </Typography>
-  </Toolbar>
-</AppBar>
-);
+class HeaderComponent extends React.Component {
+
+
+    render() {
+        let {title, logout, isLoggedIn} = this.props;
+
+        return <AppBar position="fixed" color="default">
+            <Toolbar>
+                <Typography type="title" color="inherit">
+                    {title != null ? title : ""}
+                </Typography>
+
+                <div className="grid_spacer"/>
+                <div className="grid_actions">
+                    {isLoggedIn() && <IconButton aria-label="Log Out" onTouchTap={logout}>
+                        <ExitToAppIcon/>
+                    </IconButton>}
+                </div>
+            </Toolbar>
+        </AppBar>
+    }
+}
 
 
 export default HeaderComponent;
--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -1,12 +1,28 @@
 import {connect} from 'react-redux'
-import HeaderComponent from '../components/HeaderComponent'
+import HeaderComponent from '../components/HeaderComponent';
+import {initiateServiceCall} from '../remote/services';
+import {exists} from '../util';
 
 const mapStateToProps = (state) => {
-  return Object.assign({}, state.header);
+    return Object.assign({}, state.header);
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        isLoggedIn: () =>{
+            let token = localStorage.getItem("access_token");
+            return exists(token)&&token.length>0 && token!=="null";
+        },
+        logout: ()=>{
+            localStorage.removeItem("access_token");
+            initiateServiceCall("navigation", {oldPath: "", newPath: window.location.pathname}, dispatch);
+        }
+    }
 };
 
 let Header = connect(
-  mapStateToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(HeaderComponent);
 
 export default Header;
